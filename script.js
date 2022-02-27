dbef5ceeb20a6f210a219428642b10cb70816bab
Irina Baron <irr.baron@gmail.com>
2022-02-27 21:38:21 +0200

    added function discounApp

--- script.js
+++ script.js
@@ -7,18 +7,22 @@ list = [
   {
     name: 'Ivan',
     surname: 'Ivanov',
+    quantity: 5,
   },
   {
     name: 'Georgy',
     surname: 'Georgiev',
+    quantity: 3,
   },
   {
     name: 'Todor',
     surname: 'Todorov',
+    quantity: 2,
   },
   {
     name: 'Monica',
     surname: 'Ivailova',
+    quantity: 7,
   },
 ]
 
@@ -26,12 +30,11 @@ function createList(list, value) {
   
   list.forEach( item => {
     let li = document.createElement('li');
-    let i = 1;
-    li.innerHTML = item.name + ' ' + item.surname + ' '+ ' coffee';
+    let i = item.quantity;
+    li.innerHTML = item.name + ' ' + item.surname + ' : ' + i;
 
     li.addEventListener('click', e => {
-      
-      e.target.innerHTML = item.name + ' ' + item.surname + ' '+ ' coffee';
+      e.target.innerHTML = item.name + ' ' + item.surname;
       e.target.innerHTML = e.target.textContent + ' : ' + i;
       i++;
       if(i > +value) {
@@ -39,14 +42,29 @@ function createList(list, value) {
         box.style.color = 'red';
 
         box.textContent = `Gift for this buyer: ${item.name + ' ' + item.surname}`;
+        discountApp(list, `${item.name + ' ' + item.surname}`);
         li.append(box);
       }
-
       return
     })
     ul.append(li);
 
-    
   })
 }
 createList(list, '5');
+
+function discountApp(list, winner) {
+  if(winner != undefined) {
+    console.log(`${winner} receives a gift`);
+  }
+  list.forEach( item => {
+    if(list.quantity > 3) {
+      console.log(`${item.name} + ${item.surname} receives discount 10%`);
+    }
+    if(list.quantity > 10) {
+      console.log(`${item.name} + ${item.surname} receives discount 20%`);
+    }
+
+  })
+}
+discountApp(list)
