4f828a48049890184530179f0498987ca5337f58
Irina Baron <irr.baron@gmail.com>
2022-02-27 23:02:20 +0200

    worked with functions

--- script.js
+++ script.js
@@ -5,24 +5,24 @@ wrapper.append(ul);
 
 list = [
   {
-    name: 'Ivan',
-    surname: 'Ivanov',
-    quantity: 5,
+    name: 'Nikolay',
+    surname: 'Shopov',
+    quantity: 1,
   },
   {
-    name: 'Georgy',
-    surname: 'Georgiev',
-    quantity: 3,
+    name: 'Gergana',
+    surname: 'Stoicheva',
+    quantity: 1,
   },
   {
-    name: 'Todor',
-    surname: 'Todorov',
-    quantity: 2,
+    name: 'Stoicho',
+    surname: 'Ivanchev',
+    quantity: 4,
   },
   {
-    name: 'Monica',
-    surname: 'Ivailova',
-    quantity: 7,
+    name: 'Helena',
+    surname: 'Denkova',
+    quantity: 11,
   },
 ]
 
@@ -32,6 +32,15 @@ function createList(list, value) {
     let li = document.createElement('li');
     let i = item.quantity;
     li.innerHTML = item.name + ' ' + item.surname + ' : ' + i;
+    if(i > +value) {
+      let box = document.createElement('div');
+      box.style.color = 'red';
+
+      box.textContent = `Gift for this buyer: ${item.name + ' ' + item.surname}`;
+      
+      discountApp(list, `${item.name + ' ' + item.surname}`);
+      li.append(box);
+    }
 
     li.addEventListener('click', e => {
       e.target.innerHTML = item.name + ' ' + item.surname;
@@ -57,14 +66,13 @@ function discountApp(list, winner) {
   if(winner != undefined) {
     console.log(`${winner} receives a gift`);
   }
-  list.forEach( item => {
-    if(list.quantity > 3) {
-      console.log(`${item.name} + ${item.surname} receives discount 10%`);
+  
+  for(let j = 0; j < list.length; j++) {
+    if(list[j].quantity > 3 && list[j].quantity < 10) {
+      console.log(`${list[j].name} ${list[j].surname} receives discount 10%`);
     }
-    if(list.quantity > 10) {
-      console.log(`${item.name} + ${item.surname} receives discount 20%`);
+    if(list[j].quantity > 10) {
+      console.log(`${list[j].name} ${list[j].surname} receives discount 20%`);
     }
-
-  })
+  }
 }
-discountApp(list)
